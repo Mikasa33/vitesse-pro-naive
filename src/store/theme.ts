c3abb2678e440d76d330854ae369b10da865e04b
Mikasa33 <mikasa33@qq.com>
2022-11-25 21:07:30 +0800

    feat: 添加暗黑主题

---
+++ src/store/theme.ts
@@ -0,0 +1,10 @@
+import { darkTheme } from 'naive-ui'
+import { isDark } from '~/composables/dark'
+
+export const useThemeStore = defineStore('theme', () => {
+  const naiveTheme = computed(() => isDark.value ? darkTheme : undefined);
+
+  return {
+    naiveTheme,
+  };
+});
